dde976bd14597942043b76bdd673fe73067d42a3
Ahmed Hassayoune <78177009+ahmedhassayoune@users.noreply.github.com>
2025-11-14 14:53:12 +0100

    Update site and base URL in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ahmedhassayoune.github.io',
-  base: '/portfolio',
+  site: 'https://portfolio.ahassayoune.com',
 
   vite: {
     plugins: [tailwindcss()],
